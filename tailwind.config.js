af35a9ce610320eb206af2100a5b92a2a979a41e
Jszigeti <j.szigeti1993@gmail.com>
2024-10-07 17:47:42 +0200

    Initializing the app

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#68BAF5",
+      green: "#45CB85",
+    },
+    fontFamily: {
+      khula: ["Khula", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+});
